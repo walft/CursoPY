01d973f9d6c329aefbd90a780535e0fa306f8e4a
Luis Henrique <119322648+walft@users.noreply.github.com>
2024-04-04 21:17:23 -0300

    Update Aula6.py

--- Aulas/Aula6.py
+++ Aulas/Aula6.py
@@ -1,1 +1,7 @@
 # conversão de tipos, correção type convertion, typecasting, coercion é o ato de coverter um tipo em outro tipos imutáveeis e primitivos: str, int, float, bool.
+print(1 + 1) # Dois tipos inteiros, será realizada a soma.
+print('Luis ' + 'Henrique') # Duas string será concatenado
+
+texto = '123'
+print(type(texto))
+print('Conversão do para inteiro ', texto, type(int(texto)))
