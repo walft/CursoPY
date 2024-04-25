de64bcc9f9f5f0a396049b4d7b7d0a460bc6d94e
Luis Henrique <119322648+walft@users.noreply.github.com>
2024-04-25 14:32:16 -0300

    up

---
+++ Aulas/aula13.py
@@ -0,0 +1,6 @@
+nome = 'Luis Henrique'
+altura = 1.71
+peso = 76.8
+imc = peso / altura ** 2 # IMC peso / (altura * altura)
+linha_1 = f'Olá {nome}, com a sua altura de {altura:.2f} e seu peso de {peso} seu IMC é de {imc:.2f}'
+print(linha_1)
